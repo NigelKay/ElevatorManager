ac766776d394d2699fd54ec57a31f2342bda441d
Nigel Kay <kay.webdev@gmail.com>
2019-02-10 15:02:25 +0000

    Added Lift class

---
+++ ElevatorManager/Lift.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorManager
+{
+    class Lift
+    {
+        public int ID { get; set; }
+        public bool Active { get; set; }
+        public int CurrentFloor { get; set; }
+        public int Direction { get; set; }
+        public int MaxWeight = 700;
+
+        public Lift (int id, bool active, int currentFloor, int direction, int maxWeight)
+        {
+            this.ID = id;
+            this.Active = active;
+            this.CurrentFloor = currentFloor;
+            this.Direction = direction;
+            this.MaxWeight = maxWeight;
+        }
+
+    }
+}
